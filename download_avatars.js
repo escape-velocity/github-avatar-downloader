eea746c8f90c0f4c623d311935f13e802dce813a
Bill <networker747@gmail.com>
2017-05-09 19:53:59 +0000

    got auth errors fixed with a new token key

--- download_avatars.js
+++ download_avatars.js
@@ -1,64 +1,44 @@
 var GITHUB_USER = "escape-velocity";
-var GITHUB_TOKEN = "Ybd5a919f8e36073f35e49e174d1de0828880a2a3";
+var GITHUB_TOKEN = "97b143ab994938cb249ba0075e86859f02d5eb04";
 
 var request = require('request');
 var fs = require('fs');
+
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'User-Agent': 'escape-velocity'
-  }
-};
-  request(options, cb);
-console.log(getRepoContributors);
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  console.log(options);
+  request(options, function(err, response, body){
+    if (err) { throw err;}
+    else {
+        cb(err, body);
+      }
+  });
 }
 
-getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
+getRepoContributors(repoOwner, repoName, function(err, body) {
   let parsedResults = JSON.parse(body);
   for (var i = 0; i < parsedResults.length; i++) {
-    console.log(parsedResults[i]["avatar_url"]);
+    // console.log(parsedResults[i]["avatar_url"]);
     downloadImageByURL(parsedResults[i]["avatar_url"], `avatars/${i}.jpg`)
   }
-
-    console.log("Errors:", err);
-    console.log("Result:", parsedResults);
-    console.log(parsedResults);
-});
-  function downloadImageByURL(url, filePath) {
-  request(url).pipe(fs.createWriteStream(filePath))
-// }
-// ////////////////////
-
-
-//var request = require('request');
-
-//console.log('Welcome to the GitHub Avatar Downloader!');
-
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//   url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-//   headers: {
-//     'User-Agent': "escape-velocity"
-//   }
-// };
-//   request(options, cb);
-
-// }
-
-// getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
-//   let parsedResults = JSON.parse(body);
-//   for (var i = 0; i < parsedResults.length; i++) {
-//     console.log(parsedResults[i]["avatar_url"]);
-//     downloadImageByURL(parsedResults[i]["avatar_url"], `avatars/${i}.jpg`)
-//   }
-  // console.log(parsedResults);
   // console.log("Errors:", err);
   // console.log("Result:", parsedResults);
-// });
-//   function downloadImageByURL(url, filePath) {
-//   request(url).pipe(fs.createWriteStream(filePath))
-// }
+  // console.log(parsedResults);
+});
+
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
+  console.log("URL: " + url + ", filepath: " + filePath);
+}
+
+
